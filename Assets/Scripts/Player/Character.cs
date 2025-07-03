3e8d65d3b4006d84aaf2df4a9ae437212559743c
Rafouki <rafael.miollan@gmail.com>
2025-07-03 19:09:09 +0200

    Full character controller

--- Assets/Scripts/Player/Character.cs
+++ Assets/Scripts/Player/Character.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Character : MonoBehaviour
+namespace Player
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [RequireComponent(typeof(CharacterMovementsController))]
+    public class Character : MonoBehaviour
     {
-        
-    }
+        [Header("Components")]
+        [SerializeField] private CharacterMovementsController controller;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void OnValidate()
+        {
+            if (controller == null) controller = GetComponent<CharacterMovementsController>();
+        }
+
+        private void Start()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void OnMove(InputValue value)
+        {
+            controller.moveInput = value.Get<Vector2>();
+        }
+
+        private void OnLook(InputValue value)
+        {
+            controller.lookInput = value.Get<Vector2>();
+        }
+
+        private void OnSprint(InputValue value)
+        {
+            controller.sprintInput = value.isPressed;
+        }
+
+        private void OnJump(InputValue value)
+        {
+            if (value.isPressed)
+            {
+                controller.TryJump();
+            }
+        }
     }
 }
--- Assets/Scripts/Player/CharacterMovementsController.cs
+++ Assets/Scripts/Player/CharacterMovementsController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Unity.Cinemachine;
+using UnityEngine.Events;
 
 namespace Player
 {
@@ -7,22 +8,71 @@ namespace Player
     public class CharacterMovementsController : MonoBehaviour
     {
         [Header("Movement Params")]
-        public float _maxSpeed = 4f;
+        public float maxSpeed => sprintInput ? sprintSpeed : walkSpeed;
+        public float acceleration = 30f;
+        [SerializeField] private float walkSpeed = 5f;
+        [SerializeField] private float sprintSpeed = 10f;
+        [SerializeField] private float jumpHeight = 2f;
+        [SerializeField] private bool canDoublejump = true;
+        private int timesJumped = 0;
+        public bool isSprinting
+        {
+            get
+            {
+                return sprintInput && sprintSpeed > 0.1f;
+            }
+        }
+
+        [Header("Looking Params")]
+        public Vector2 lookSensitivity = new Vector2(0.1f, 0.1f);
+        public float pitchLimit = 85f;
+        [SerializeField] private float currentPitch = 0f;
+        public float CurrentPitch
+        {
+            get => currentPitch;
+            set
+            {
+                currentPitch = Mathf.Clamp(value, -pitchLimit, pitchLimit);
+            }
+        }
+
+        [Header("Camera Params")]
+        [SerializeField] private float cameraNoramlFOV = 75f;
+        [SerializeField] private float cameraSprintFOV = 90f;
+        [SerializeField] private float cameraFOVSmoothing = 5f;
+        private float targetCameraFOV
+        {
+            get
+            {
+                return isSprinting ? cameraSprintFOV : cameraNoramlFOV;
+            }
+        }
+
+        [Header("Physics Params")]
+        [SerializeField] private float gravityScale = 2f;
+        public float verticalVelocity = 0f;
+        public Vector3 currentVelocity { get; private set; }
+        public float currentSpeed { get; private set; }
+        public bool isgrounded => characterController.isGrounded;
+        private bool wasGrounded = false;
 
         [Header("Input")]
-        public Vector2 _moveInput;
-        public Vector2 _lookInput;
+        public Vector2 moveInput;
+        public Vector2 lookInput;
+        public bool sprintInput;
 
         [Header("Components")]
-        [SerializeField] CinemachineCamera _FpCamera;
-        [SerializeField] CharacterController _characterController;
+        [SerializeField] private CinemachineCamera fpCamera;
+        [SerializeField] private CharacterController characterController;
 
-        #region Unity Method
+        [Header("Events")]
+        public UnityEvent landed;
+
         private void OnValidate()
         {
-            if ( _characterController == null)
+            if ( characterController == null)
             {
-                _characterController = GetComponent<CharacterController>();
+                characterController = GetComponent<CharacterController>();
             }
         }
 
@@ -30,24 +80,79 @@ namespace Player
         {
             MoveUpdate();
             LookUpdate();
-        }
-        #endregion
+            CameraUpdate();
 
-        #region Controller Methods
-        void MoveUpdate()
+            if (!wasGrounded && isgrounded)
+            {
+                timesJumped = 0;
+                landed?.Invoke();
+            }
+
+            wasGrounded = isgrounded;
+        }
+
+        public void TryJump()
         {
-            Vector3 motion = transform.forward * _moveInput.y + transform.right * _moveInput.x;
+            if (!isgrounded)
+            {
+                if (!canDoublejump || timesJumped >= 2)
+                {
+                    return;
+                }
+            }
+
+            verticalVelocity = Mathf.Sqrt(jumpHeight * -2 * Physics.gravity.y * gravityScale);
+            timesJumped++;
+        }
+
+        private void MoveUpdate()
+        {
+            Vector3 motion = transform.forward * moveInput.y + transform.right * moveInput.x;
             motion.y = 0f;
             motion.Normalize();
 
-            _characterController.Move(motion * _maxSpeed * Time.deltaTime);
+            if (motion.sqrMagnitude >= 0.01f )
+            {
+                currentVelocity = Vector3.MoveTowards(currentVelocity, motion * maxSpeed, acceleration * Time.deltaTime);
+            }
+            else
+            {
+                currentVelocity = Vector3.MoveTowards(currentVelocity, Vector3.zero, acceleration * Time.deltaTime);
+            }
+
+            if (isgrounded && verticalVelocity <= 0.01f)
+            {
+                verticalVelocity = -3f;
+            }
+            else
+            {
+                verticalVelocity += Physics.gravity.y * gravityScale * Time.deltaTime;
+            }
+
+            Vector3 fullVelocity = new Vector3(currentVelocity.x, verticalVelocity, currentVelocity.z);
+
+            CollisionFlags flags = characterController.Move(fullVelocity * Time.deltaTime);
+
+            if ((flags & CollisionFlags.Above) != 0 && verticalVelocity > 0f)
+            {
+                Debug.Log("Fait gaffe à ta tête");
+                verticalVelocity = 0f;
+            }
+            currentSpeed = currentVelocity.magnitude;
         }
 
-        void LookUpdate()
+        private void LookUpdate()
         {
-
+            Vector2 input = new Vector2(lookInput.x * lookSensitivity.x, lookInput.y * lookSensitivity.y);
+            CurrentPitch -= input.y;
+            fpCamera.transform.localRotation = Quaternion.Euler(CurrentPitch, 0f, 0f);
+            transform.Rotate(Vector3.up * input.x);
         }
-        #endregion
+
+        private void CameraUpdate()
+        {
+            fpCamera.Lens.FieldOfView = Mathf.Lerp(fpCamera.Lens.FieldOfView, targetCameraFOV, cameraFOVSmoothing * Time.deltaTime);
+        }
     }
 }
 
